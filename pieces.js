af83b26ea93c03f8313f80d4e260117731af6ce9
Timothé Cordier <timothe.cordier@gmail.com>
2024-01-21 17:07:32 +0100

    mise à jour pieces.js

--- pieces.js
+++ pieces.js
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
-// Création du lien avec le fichier pieces-auto.json
 // Récupération des pièces depuis le fichier JSON
 const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
 
 // Création du lien entre json et js
 const article = pieces[0];
-=======
-// Récupération des pièces depuis le fichier JSON
-const reponse = await fetch('pieces-autos.json');
-const pieces = await reponse.json();
-
-// Création des balises 
-const article = pieces[2];
->>>>>>> P1C4-Exercice
 
 const imageElement = document.createElement("img");
 imageElement.src = article.image;
@@ -25,17 +15,6 @@ const prixElement = document.createElement("p");
 prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
 
 const categorieElement = document.createElement("p");
-<<<<<<< HEAD
-categorieElement.innerText = article.categorie ?? "(Aucune catégorie)"
-
-// Création du lien entre js et html
-
-const sectionFiches = document.querySelector(".fiches")
-sectionFiches.appendChild(imageElement)
-sectionFiches.appendChild(nomElement)
-sectionFiches.appendChild(prixElement)
-sectionFiches.appendChild(categorieElement)
-=======
 categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
 
 const descriptionElement = document.createElement("p")
@@ -52,4 +31,3 @@ sectionFiches.appendChild(prixElement);
 sectionFiches.appendChild(categorieElement);
 sectionFiches.appendChild(descriptionElement);
 sectionFiches.appendChild(disponibiliteElement);
->>>>>>> P1C4-Exercice
