acae415bc5895224775df911f7d2b0821bc52275
Timothé Cordier <timothe.cordier@gmail.com>
2024-01-22 13:34:24 +0100

    lal

--- pieces.js
+++ pieces.js
@@ -2,47 +2,6 @@
 const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
 
-<<<<<<< HEAD
-
-for (let i = 0; i < pieces.length; i++) {
-    
-        const article = pieces[i];
-        // Récupération de l'élément du DOM qui accueillera les fiches
-        const sectionFiches = document.querySelector(".fiches");
-        // Création d’une balise dédiée à une pièce automobile
-        const pieceElement = document.createElement("article");
-        // On crée l’élément img.
-        const imageElement = document.createElement("img");
-        imageElement.src = article.image;
-        // Création du lien entre json et js
-        const nomElement = document.createElement("h2");
-        nomElement.innerText = article.nom;
-
-        const prixElement = document.createElement("p");
-        prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
-
-        const categorieElement = document.createElement("p");
-        categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
-
-        const descriptionElement = document.createElement("p")
-        descriptionElement.innerText = article.description ?? "Pas de description pour le moment"
-
-        const disponibiliteElement = document.createElement("p")
-        disponibiliteElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock" 
-
-        //Rattachement de nos balises au DOM
-        sectionFiches.appendChild(pieceElement);
-        pieceElement.appendChild(imageElement);
-        pieceElement.appendChild(nomElement);
-        pieceElement.appendChild(prixElement);
-        pieceElement.appendChild(categorieElement);
-        pieceElement.appendChild(descriptionElement);
-        pieceElement.appendChild(disponibiliteElement);
-
-}
-
-
-=======
 for (let i = 0; i < pieces.length; i++) {
 
     const article = pieces[i];
@@ -95,4 +54,3 @@ for (let i = 0; i < pieces.length; i++) {
  })
 
  
->>>>>>> P2C1-BoucleFor
